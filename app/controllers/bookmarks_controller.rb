f6f03914d5eb43bb9ee56704f48c31441648f5d9
yonathan-palma <yonathanpalmarey@gmail.com>
2024-02-26 14:38:11 -0400

    controlles_complete

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,37 @@
+class BookmarksController < ApplicationController
+  # before_action :find_bookmark, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = List.find(params[:list_id])
+    if @bookmark.save
+      redirect_to list_path(params[:list_id])
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path
+  end
+
+  private
+
+  def find_bookmark
+    @bookmark = Bookmark.new
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,27 @@
 Rails.application.routes.draw do
+  get 'bookmarks/index'
+  get 'bookmarks/new'
+  get 'lists/list'
+  get 'lists/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
+
+  # get 'lists' => 'lists#index'
+  # get 'lists/new' => 'lists#new'
+  # get 'lists/:id' => 'lists#show'
+  # post 'lists' => 'list#create'
+
+  get 'lists' => 'lists#index'
+  resources :lists, only: [:new, :create, :show] do
+    resources :bookmarks, only: [:index, :new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 
   # Defines the root path route ("/")
   # root "posts#index"
 end
+
+# root to: "lists#index"
